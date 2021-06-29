bd045e252fd132b6845489d4ab0c618e0a93acd5
hbelmiro <helber.belmiro@gmail.com>
2021-06-29 08:35:36 -0300

    Methods for Objects

---
+++ jdk9/src/main/java/com/thegreatapi/newjdkfeatures/jdk9/MethodsForObjects.java
@@ -0,0 +1,42 @@
+package com.thegreatapi.newjdkfeatures.jdk9;
+
+import javax.annotation.Nullable;
+import java.util.Objects;
+
+@SuppressWarnings("unused")
+public interface MethodsForObjects {
+
+    String requireNonNullElse(@Nullable String value);
+
+    String requireNonNullElseGet(@Nullable String value);
+
+    class Jdk8 implements MethodsForObjects {
+
+        @Override
+        public String requireNonNullElse(@Nullable String value) {
+            return value != null ? value : getDefaultValue();
+        }
+
+        @Override
+        public String requireNonNullElseGet(@Nullable String value) {
+            return value != null ? value : getDefaultValue();
+        }
+    }
+
+    class Jdk9 implements MethodsForObjects {
+
+        @Override
+        public String requireNonNullElse(@Nullable String value) {
+            return Objects.requireNonNullElse(value, "default value");
+        }
+
+        @Override
+        public String requireNonNullElseGet(@Nullable String value) {
+            return Objects.requireNonNullElseGet(value, this::getDefaultValue);
+        }
+    }
+
+    default String getDefaultValue() {
+        return "default value";
+    }
+}
