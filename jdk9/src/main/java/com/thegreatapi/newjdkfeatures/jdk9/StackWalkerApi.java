d103e0641edc9cb94aab44e08ce92f15b334049a
hbelmiro <helber.belmiro@gmail.com>
2021-06-29 08:10:03 -0300

    StackWalker API

---
+++ jdk9/src/main/java/com/thegreatapi/newjdkfeatures/jdk9/StackWalkerApi.java
@@ -0,0 +1,50 @@
+package com.thegreatapi.newjdkfeatures.jdk9;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static java.lang.StackWalker.Option.RETAIN_CLASS_REFERENCE;
+
+@SuppressWarnings("unused")
+public interface StackWalkerApi {
+
+    List<Class<?>> INTERESTING_CLASSES = List.of();
+
+    Optional<Class<?>> findClassInStack();
+
+    class Jdk8 implements StackWalkerApi {
+
+        @Override
+        public Optional<Class<?>> findClassInStack() {
+            List<String> interestingClassNames = INTERESTING_CLASSES.stream()
+                                                                    .map(Class::getName)
+                                                                    .collect(Collectors.toList());
+
+            return Arrays.stream(Thread.currentThread()
+                                       .getStackTrace())
+                         .map(StackTraceElement::getClassName)
+                         .filter(interestingClassNames::contains)
+                         .findFirst()
+                         .map(className -> {
+                             try {
+                                 return Class.forName(className);
+                             } catch (ClassNotFoundException e) {
+                                 throw new RuntimeException(e);
+                             }
+                         });
+        }
+    }
+
+    class Jdk9 implements StackWalkerApi {
+
+        @Override
+        public Optional<Class<?>> findClassInStack() {
+            return StackWalker.getInstance(RETAIN_CLASS_REFERENCE).walk(stackFrameStream ->
+                    stackFrameStream.<Class<?>>map(StackWalker.StackFrame::getDeclaringClass)
+                                    .filter(INTERESTING_CLASSES::contains)
+                                    .findFirst());
+        }
+    }
+}
