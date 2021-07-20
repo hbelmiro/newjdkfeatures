0d7eb63048978ba2c7e2980c72d5f92fc34b116b
hbelmiro <helber.belmiro@gmail.com>
2021-07-20 08:24:08 -0300

    Var

--- jdk10/src/main/java/com/thegreatapi/newjdkfeatures/jdk10/Var.java
+++ jdk10/src/main/java/com/thegreatapi/newjdkfeatures/jdk10/Var.java
@@ -1,32 +1,38 @@
 package com.thegreatapi.newjdkfeatures.jdk10;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
+@SuppressWarnings({"unused", "EmptyTryBlock"})
 public interface Var {
 
-    void doStuff() throws Exception;
+    void doStuff() throws IOException;
 
     class Jdk9 implements Var {
 
         @Override
-        public void doStuff() throws Exception {
+        public void doStuff() throws IOException {
+            Map<Integer, String> map = new HashMap<>();
             ExecutorService executorService = Executors.newSingleThreadExecutor();
-
-            Future<Integer> futureValue = executorService.submit(() -> 42);
-            System.out.println(futureValue.get());
+            try (ByteArrayOutputStream outputStream = new ByteArrayOutputStream("anyString".getBytes().length)) {
+                //...
+            }
         }
     }
 
     class Jdk10 implements Var {
 
         @Override
-        public void doStuff() throws Exception {
+        public void doStuff() throws IOException {
+            var map = new HashMap<Integer, String>();
             var executorService = Executors.newSingleThreadExecutor();
-
-            Future<Integer> futureValue = executorService.submit(() -> 42);
-            System.out.println(futureValue.get());
+            try (var outputStream = new ByteArrayOutputStream("anyString".getBytes().length)) {
+                //...
+            }
         }
     }
 }
