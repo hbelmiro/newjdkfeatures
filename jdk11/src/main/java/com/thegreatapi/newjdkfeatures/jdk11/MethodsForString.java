2c2403085d329ea2589ccf09ce70512c69055985
hbelmiro <helber.belmiro@gmail.com>
2021-07-23 09:53:17 -0300

    JDK 11 - MethodsForString - Added repeat

--- jdk11/src/main/java/com/thegreatapi/newjdkfeatures/jdk11/MethodsForString.java
+++ jdk11/src/main/java/com/thegreatapi/newjdkfeatures/jdk11/MethodsForString.java
@@ -4,11 +4,22 @@ public interface MethodsForString {
 
     boolean isBlank(String s);
 
+    String repeat(String s);
+
     class Jdk10 implements MethodsForString {
 
         @Override
         public boolean isBlank(String s) {
             return s.trim().isEmpty();
+        }
+
+        @Override
+        public String repeat(String s) {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < 4; i++) {
+                stringBuilder.append(s);
+            }
+            return stringBuilder.toString();
         }
     }
 
@@ -17,6 +28,11 @@ public interface MethodsForString {
         @Override
         public boolean isBlank(String s) {
             return s.isBlank();
+        }
+
+        @Override
+        public String repeat(String s) {
+            return s.repeat(4);
         }
     }
 }
