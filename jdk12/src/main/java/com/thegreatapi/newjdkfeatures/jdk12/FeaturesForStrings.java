bbc61449ace038fa9785cb205ef23a8f942901f8
hbelmiro <helber.belmiro@gmail.com>
2021-07-23 09:58:51 -0300

    JDK 12 - FeaturesForString - Indent

--- jdk12/src/main/java/com/thegreatapi/newjdkfeatures/jdk12/FeaturesForStrings.java
+++ jdk12/src/main/java/com/thegreatapi/newjdkfeatures/jdk12/FeaturesForStrings.java
@@ -1,8 +1,23 @@
 package com.thegreatapi.newjdkfeatures.jdk12;
 
+@SuppressWarnings({"StringRepeatCanBeUsed", "unused"})
+@FunctionalInterface
 public interface FeaturesForStrings {
 
     String indentation(String s, int indentationSize);
+
+    class Jdk8 implements FeaturesForStrings {
+
+        @Override
+        public String indentation(String s, int indentationSize) {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < indentationSize; i++) {
+                stringBuilder.append(' ');
+            }
+            stringBuilder.append(s);
+            return stringBuilder.toString();
+        }
+    }
 
     class Jdk11 implements FeaturesForStrings {
 
